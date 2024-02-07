6a632519d149964c36c7402d3d8121d2a4d3bc9b
PMW9905 <pmw9905@gmail.com>
2024-02-06 22:04:45 -0600

    Changed apartment details to square footage

--- apartment_data.py
+++ apartment_data.py
@@ -5,8 +5,8 @@ class ApartmentData:
     unit_number: str
     layout_name: str
     cost: str
-    unit_details: str
+    square_footage: str
     availability: str
 
     def print_all_data(self):
-        print(f'{self.unit_number} | {self.layout_name} | {self.cost} | {self.unit_details} | {self.availability}')
+        print(f'{self.unit_number} | {self.layout_name} | {self.cost} | {self.square_footage} | {self.availability}')
